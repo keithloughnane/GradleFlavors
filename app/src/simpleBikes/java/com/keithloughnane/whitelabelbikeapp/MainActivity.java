7a22fc6b9d5da33cf10717fa705f276b1e618863
keith.loughnane <keith.loughnane@gmail.com>
2018-04-13 21:22:56 +0300

    minor cleanup

--- app/src/simpleBikes/java/com/keithloughnane/whitelabelbikeapp/MainActivity.java
+++ app/src/simpleBikes/java/com/keithloughnane/whitelabelbikeapp/MainActivity.java
@@ -2,7 +2,7 @@ package com.keithloughnane.whitelabelbikeapp;
 
 
 public class MainActivity extends BaseMainActivity {
-    /*Strictly speaking there is no need to extend this class
-      but I'm imagining we have a white lable app and this customer has more fuctionality here.
+    /* Strictly speaking there is no need to extend this class
+       but I'm imagining we have a white label app and this customer has morfunctionality here.
      */
 }
